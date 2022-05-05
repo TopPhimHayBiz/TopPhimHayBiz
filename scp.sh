022aa78bffa59398fd75c45a795c53a7032d8a09
TopThang <63397623+TopThangCom@users.noreply.github.com>
2022-05-05 12:05:01 +0700

    Add files via upload

---
+++ scp.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/expect -f
+
+spawn bash -c "scp  -r *.txt.zip  root@23.88.121.231:/root/fr/"
+expect {
+  -re ".*es.*o.*" {
+    exp_send "yes\r"
+    exp_continue
+  }
+  -re ".*sword.*" {
+    exp_send "12345678\r"
+  }
+}
+interact
